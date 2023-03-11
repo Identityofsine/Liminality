11b0e271db456bede693d5e03e4db377e1a1d554
Kevin <identityofsine@gmail.com>
2023-03-10 20:02:43 -0500

    added tracer

--- src/pages/titlescreen/TitleScreen.jsx
+++ src/pages/titlescreen/TitleScreen.jsx
@@ -79,10 +79,10 @@ const stories = [
 
 
 
-function OpenedStory({story = {name:'',image:'',imagebanner:''}}) {
+function OpenedStory({story = {name:'',image:'',imagebanner:''}, condense = () => {}}) {
   return(
     <div className='opened-story'>
-      <div className='collapse'>
+      <div className='collapse' onClick={condense}>
         <img src='/icons/collapse.svg' alt='close'/>
       </div>
       <div className='left card'>
@@ -129,7 +129,14 @@ function TitleScreen() {
 
   const [currentStory, setStory] = useState(stories[0]);
   const [clicked, setClicked] = useState(false);
+  const [storyExpanded, setExpand] = useState({expanded:true, story:stories[0]})
 
+
+  const handleExpand = () => {
+    var temp = {...storyExpanded};
+    temp.expanded = !temp.expanded;
+    setExpand(temp)
+  }
 
   const handleStoryClick = (e) => {
     if(clicked && currentStory === e){
@@ -142,14 +149,14 @@ function TitleScreen() {
 
   return (
     <div className='title-container'>
-      {/* <OpenedStory/> */}
+      {storyExpanded.expanded && <OpenedStory condense={handleExpand}/>}
       <div className='card left'>
         <div className='card-container'>
           <div className='title'>
             <span>Explore:</span>
           </div>
           <div className={`story-container`}>
-            {stories.map((d,i) => (<div className={`story ${clicked && currentStory === d ? 'forcehover' : ''}`} onClick={() => {handleStoryClick(d)}} onMouseEnter={() => {if(clicked) return; setStory(d)}} style={{backgroundImage:`url("${d.image}")`}}/>))}
+            {stories.map((d,i) => (<div data-type='story' className={`inter story ${clicked && currentStory === d ? 'forcehover' : ''}`} onClick={() => {handleStoryClick(d)}} onMouseEnter={() => {if(clicked) return; setStory(d)}} style={{backgroundImage:`url("${d.image}")`}}/>))}
             
           </div>
         </div>
@@ -157,7 +164,7 @@ function TitleScreen() {
       <div className='line'/>
       <div className='card right'>
         <div className='card-container'>
-          <div className='expand'>
+          <div className='inter expand' data-type='expand' onClick={handleExpand}>
             <img src='/icons/expand.svg' alt='expand'/>
           </div>
           <div className='title'>
