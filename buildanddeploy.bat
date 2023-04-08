816b6f780d640e8469ebb45765ce91c5ca7365ef
Identityofsine <67929513+Identityofsine@users.noreply.github.com>
2023-04-08 00:15:17 -0400

    Fixed Batch File

--- buildanddeploy.bat
+++ buildanddeploy.bat
@@ -13,11 +13,21 @@ if %does_exist% equ 0 (
 	echo Invalid Directory
 	goto :EDIT
 )
+IF "%CD%"=="%newDIR%" (
+	echo You cannot use this directory as your target
+	GOTO :EDIT
+) ELSE (
+	IF "%newDIR%"=="." (
+	echo You cannot use this directory as your target
+	GOTO :EDIT
+	)
+)
+
 set does_exist=0
 CALL :DOESEXIST %does_exist%
 if %does_exist% equ 1 rm .env.batch
 echo %newDIR%> .env.batch
-goto :INIR
+goto :INIT
 
 
 :DIREXIST
@@ -42,10 +52,13 @@ echo Moving New Build to %dir%...
 xcopy /E /H /C /I /Y .\build\* %dir%\ 
 cd %dir%
 echo Pushing to GitHub
+
+
 git add .
-git commit -m "Basic Commit"
+git commit -m "%data% - Auto Commit"
 git push
 
+echo Commited...
 
 goto :EOF
 
