ed8b73682a889a6792e0e99e2eecd2270325da7c
Identityofsine <67929513+Identityofsine@users.noreply.github.com>
2023-04-07 23:47:31 -0400

    Basic Commit

---
+++ buildanddeploy.bat
@@ -0,0 +1,61 @@
+@echo off
+
+setlocal
+:PROMPT
+SET /P CHANGEDIR="Edit Directory? [y,n]"
+IF /I "%CHANGEDIR%" NEQ "y" IF /I "%CHANGEDIR%" NEQ "Y" GOTO INIT ELSE GOTO EDIT
+
+:EDIT
+SET /P newDIR="New Directory: "
+set does_exist=0
+CALL :DIREXIST %newDIR%, does_exist
+if %does_exist% equ 0 (
+	echo Invalid Directory
+	goto :EDIT
+)
+set does_exist=0
+CALL :DOESEXIST %does_exist%
+if %does_exist% equ 1 rm .env.batch
+echo %newDIR%> .env.batch
+goto :INIR
+
+
+:DIREXIST
+if exist %~1 (
+	set "%~2=1"	
+) else (
+	set "%~2=0"
+)
+goto :EOF
+
+:INIT
+set does_exist=0
+CALL :DOESEXIST does_exist
+if %does_exist% equ 0 GOTO EDIT
+
+set /p dir=<.env.batch
+
+echo "INIT"
+start npm run build
+echo Removing Old Contents from %dir%...
+start del /Q /F dir\* 
+echo Moving New Build to %dir%...
+start mv .\build\* dir\
+cd dir
+echo Pushing to GitHub
+start git add .
+start git commit -m "Basic Commit"
+start git push
+
+
+goto :EOF
+
+:DOESEXIST
+if exist .env.batch (
+	set %1=1
+) else (
+	set %1=0
+)
+goto :EOF
+
+	
