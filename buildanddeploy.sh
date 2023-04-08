7898df9e5714bd9e26c7f702a1b3bb8583544990
Kevin <identityofsine@gmail.com>
2023-04-07 21:50:18 -0400

    Added bash script to build into repo folder

---
+++ buildanddeploy.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+read -n1 -p "Edit Directory? [y,n]" inputvar
+
+function isValidDirectory(){
+    if [ -d "$1" ]
+    then
+        return 0
+    else 
+        return 1
+    fi
+}
+function fileExist(){
+    if [ -f "$1" ]; then
+        return 0
+    else 
+        return 1
+    fi
+}
+
+function run_init() {
+    local dir=$(getDirectory);
+    echo -e "\nStoring Build Into : $dir";
+    if isValidDirectory $dir
+    then
+        echo -e "\nValid Directory \n"
+    else
+        echo -e "\nInvalid Directory -- Please Change\n"
+        return
+    fi
+    pwd curDir;
+    npm run build;
+    sleep 5;
+    rm -rf $dir/*;
+    mv ./build/* $dir/;
+    echo Moved Build to $dir;
+
+}
+
+function editDirectory () {
+    local newdir
+    declare -i result=1
+    echo -e "\n";
+    while ! isValidDirectory "$newdir"; 
+    do
+        read -p "Enter a Directory : " newdir;
+        if ! isValidDirectory "$newdir"; then
+            echo -e "\nInvalid Directory";
+        fi
+    done    
+    if fileExist ".env.bash"; then
+        rm .env.bash;
+    fi
+    echo $newdir > .env.bash;
+    run_init;
+}
+
+function getDirectory () {
+    if !fileExist ".env.bash"; then
+        echo -e "\n.env.bash missing... redirecting";
+        editDirectory;
+    fi
+    input=".env.bash";
+    local stdout
+    while IFS= read -r line; do
+        stdout="$line"
+    done < "$input"
+    echo "$stdout";
+}
+
+
+
+case $inputvar in
+    y|Y) editDirectory ;;
+    n|N) run_init;;
+    *) echo "Exit" ;;
+esac
